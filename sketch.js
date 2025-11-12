a7304a8a702ef43f515b00f865410e817f02a6b1
seun <seun.edagolota@gmail.com>
2025-11-12 15:43:03 -0500

    simple drawing on empty canvas

--- sketch.js
+++ sketch.js
@@ -1,7 +1,21 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(500, 500);
 }
 
 function draw() {
-  background(220);
+  
+  
+  if(mouseIsPressed === true){
+    fill(0);
+    
+    //circle(mouseX, mouseY, 100);
+    line(mouseX, mouseY, mouseX, mouseY);
+    strokeWeight(10);
+    stroke('magenta')
+  
+  }
+
+
+
+ 
 }
