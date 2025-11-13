7d549ba8f59dcee2289d6fc169f64a57799bccc0
seun <seun.edagolota@gmail.com>
2025-11-12 21:55:16 -0500

    spherical cube

--- sketch.js
+++ sketch.js
@@ -1,21 +1,57 @@
+let spacing = 25;
+let r = spacing/2;
+let number; let divider;
+let angle = 0;
+
 function setup() {
-  createCanvas(500, 500);
+  createCanvas(500, 500, WEBGL);
+
+  angleMode(DEGREES);
+  number = createSlider(1, 10, 5);
+  number.position(10,10);
+  number.size(80);
+
+  divider = createSlider(1, 10, 3, 0.5);
+  divider.position(100,10);
+  divider.size(80);
 }
 
 function draw() {
+  background(220);
+
+  let num = number.value();
+  let d = divider.value();
+
+  orbitControl();
+  rotateX(-15);
+  rotateY(angle);
   
-  
-  if(mouseIsPressed === true){
-    fill(0);
+  stroke(0);
+  for(let i = 0; i < num; i++){ 
+    for(let j = 0; j<num; j++){
+      for(let k = 0; k<num; k++){
+        push();
+        let offset = -spacing * num/2 + spacing/2;
+      let x = i*spacing + offset;
+      let y = j*spacing + offset;
+      let z = k*spacing + offset;
+
+      let distance = sqrt(pow(x, 2) + pow(y,2) + pow(z,2))
+      translate(x,y,z);
+      normalMaterial();
+      let sphereSize = spacing - distance/d;
+      sphere(sphereSize);
+      pop();
+      }
+      
+    }
     
-    //circle(mouseX, mouseY, 100);
-    line(mouseX, mouseY, mouseX, mouseY);
-    strokeWeight(10);
-    stroke('magenta')
-  
   }
+angle+=0.1;
+  stroke('red');
 
-
+  sphere(num);
+  
 
  
 }
